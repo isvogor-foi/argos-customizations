1badfe911a944fe25870c8d18c69b82971997a5b
isvogor <isvogor@gmail.com>
2017-01-30 10:13:24 -0500

    init

---
+++ e-pucky/CMakeFiles/argos3plugin_simulator_epucky.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/argos3plugin_simulator_epucky.dir/simulator/dynamics2d_epucky_model.cpp.o"
+  "CMakeFiles/argos3plugin_simulator_epucky.dir/simulator/epucky_entity.cpp.o"
+  "CMakeFiles/argos3plugin_simulator_epucky.dir/simulator/qtopengl_epucky.cpp.o"
+  "libargos3plugin_simulator_epucky.pdb"
+  "libargos3plugin_simulator_epucky.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/argos3plugin_simulator_epucky.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
